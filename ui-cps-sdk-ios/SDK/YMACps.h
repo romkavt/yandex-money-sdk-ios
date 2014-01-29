73365b1077a4999e414f28a312cc395b44e7723f
Александр Мертвецов <a.mertvetsov@gmail.com>
2014-01-29 22:51:45 +0400

    YMACps added

---
+++ ui-cps-sdk-ios/SDK/YMACps.h
@@ -0,0 +1,15 @@
+//
+//  YMACps.h
+//  ui-cps-sdk-ios
+//
+//  Created by Александр Мертвецов on 29.01.14.
+//  Copyright (c) 2014 Yandex.Money. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <ymcpssdk-ios/YMASession.h>
+
+@interface YMACps : NSObject
+
+
+@end
---
+++ ui-cps-sdk-ios/SDK/YMACps.m
@@ -0,0 +1,14 @@
+//
+//  YMACps.m
+//  ui-cps-sdk-ios
+//
+//  Created by Александр Мертвецов on 29.01.14.
+//  Copyright (c) 2014 Yandex.Money. All rights reserved.
+//
+
+#import "YMACps.h"
+
+@implementation YMACps
+
+
+@end
